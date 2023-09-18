70ff6667af614d3fc89dd990a1cd512b51667a6a
Dobri.Ivanov <dobri0316@abv.bg>
2023-09-18 14:15:48 +0300

    Added create category functionality

--- ProductStore/Controllers/CategoryController.cs
+++ ProductStore/Controllers/CategoryController.cs
@@ -1,21 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using ProductStore.Data;
 using ProductStore.Models;
 
 namespace ProductStore.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private readonly ProductStoreDbContext _data;
-        public CategoryController(ProductStoreDbContext data)
-        {
-            _data = data;
-        }
-        public async Task<IActionResult> Index()
-        {
-            List<Category> categories = await _data.Categories.ToListAsync();
-            return View(categories);
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private readonly ProductStoreDbContext _data;
+
+		public CategoryController(ProductStoreDbContext data)
+		{
+			_data = data;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			List<Category> categories = _data.Categories.ToList();
+			return View(categories);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{			
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			_data.Categories.Add(obj);
+			_data.SaveChangesAsync();
+
+			return RedirectToAction("Index");
+		}
+	}
 }
