1a172a22661be699ef5a2d18188461022b583b2c
Dobri-Ivanov <dobri0316@abv.bg>
2023-09-18 15:34:08 +0300

    Added delete functionality, Added toastr notifications

--- ProductStore/Controllers/CategoryController.cs
+++ ProductStore/Controllers/CategoryController.cs
@@ -42,6 +42,7 @@ namespace ProductStore.Controllers
 			_data.Categories.Add(obj);
 			_data.SaveChangesAsync();
 
+			TempData["success"] = "A new category is successfully created!";
 			return RedirectToAction("Index");
 		}
 
@@ -53,7 +54,7 @@ namespace ProductStore.Controllers
 				return NotFound();
 			}
 
-			Category obj = _data.Categories.Find(id);
+			Category? obj = _data.Categories.Find(id);
 
 			if (obj == null)
 			{
@@ -66,6 +67,11 @@ namespace ProductStore.Controllers
 		[HttpPost]
 		public IActionResult Edit(Category obj)
 		{
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
 			if (!ModelState.IsValid)
 			{
 				return View(obj);
@@ -74,6 +80,40 @@ namespace ProductStore.Controllers
 			_data.Categories.Update(obj);
 			_data.SaveChanges();
 
+			TempData["success"] = "Category is successfully updated!";
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			Category? obj = _data.Categories.Find(id);
+
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			return View(obj);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(Category obj)
+		{
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			_data.Categories.Remove(obj);
+			_data.SaveChanges();
+
+			TempData["success"] = "Category is successfully deleted!";
 			return RedirectToAction("Index");
 		}
 	}
