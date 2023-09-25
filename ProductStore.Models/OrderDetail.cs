ff3de0225a929e72adb406a8228865a268c5349c
Dobri-Ivanov <dobri0316@abv.bg>
2023-09-25 10:48:15 +0300

    Added order models

---
+++ ProductStore.Models/OrderDetail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStore.Models
+{
+	public class OrderDetail
+	{
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey(nameof(OrderHeader))]
+        public int OrderHeaderId { get; set; }
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
